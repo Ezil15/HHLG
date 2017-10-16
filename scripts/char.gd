24611e7e4e450b7f657fba8e76823f4f3b254a91
Ezil15 <izhevago@mail.ru>
2017-10-16 22:05:47 +0300

    -Добавил пару классов для сущностей и доработал несколько строк.

--- scripts/char.gd
+++ scripts/char.gd
@@ -1,4 +1,12 @@
 extends KinematicBody2D
+var health = 100
+var stamina = 100
+
+
+var fireResist = 0.15
+var aquaResist = 0.15
+var Physic = 0.05
+
 var WALK_SPEED = 250
 var Class = preload("res://scripts/charClass.gd").new()
 func _ready():
@@ -6,6 +14,12 @@ func _ready():
 	print(str(Class.charClass))
 	set_fixed_process(true)
 func _fixed_process(delta):
+	Moving(delta)
+	
+	
+	
+	
+func Moving(del):
 	var movePos = Vector2()
 	if Input.is_action_pressed("walk_up"):
 		movePos.y -= 1
@@ -16,5 +30,5 @@ func _fixed_process(delta):
 	if Input.is_action_pressed("walk_right"):
 		movePos.x += 1
 		
-	movePos = movePos.normalized()*WALK_SPEED*delta
+	movePos = movePos.normalized()*WALK_SPEED*del
 	move(movePos)
---
+++ scripts/entity.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+var health = 100
+var stamina = 100
+var fireResist = 0.15
+var aquaResist = 0.15
+var Physic = 0.05
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
